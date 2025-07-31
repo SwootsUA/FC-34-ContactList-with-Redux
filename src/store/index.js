6aef11308a737b444288305fb071f20dda8de4ad
Kostiantyn <kostya20022210@gmail.com>
2025-07-31 16:13:48 +0300

    Add redux-logger and redux-devtools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevToolsDevelopmentOnly} from '@redux-devtools/extension';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-export default createStore(rootReducer);
+export default createStore(
+    rootReducer,
+    composeWithDevToolsDevelopmentOnly(applyMiddleware(logger))
+);
